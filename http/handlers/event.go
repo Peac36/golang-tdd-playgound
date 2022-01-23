0a2f6ca0085587629fce71863935de3e9567fcbf
nstoqnov <nstoqnov@2create.studio>
2022-01-23 20:43:15 +0200

    Introduce the events handler file

--- http/handlers/event.go
+++ http/handlers/event.go
@@ -91,33 +91,3 @@ func GetEvent(connection *gorm.DB) http.Handler {
 
 	})
 }
-
-func GetEvents(connection *gorm.DB, t security.TokenSecurity) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			responses.NewJsonResponse(rw, http.StatusMethodNotAllowed, nil)
-			return
-		}
-
-		token := r.Header.Get(middlewares.AuthorizationHeader)
-		identifier, err := t.GetIdentifier(token)
-		if err != nil {
-			responses.NewJsonResponse(rw, http.StatusUnauthorized, nil)
-			return
-		}
-
-		user := models.User{}
-		result := connection.Preload("Events").Find(&user, identifier)
-		if result.Error != nil {
-			responses.NewJsonResponse(rw, http.StatusInternalServerError, nil)
-			return
-		}
-
-		if user.ID == 0 {
-			responses.NewJsonResponse(rw, http.StatusNotFound, nil)
-			return
-		}
-
-		responses.NewJsonResponse(rw, http.StatusOK, user.Events)
-	})
-}
